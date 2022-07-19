fbe87c7f393eda6ed3f6e461dc25eb442fbf0f79
jaujau088 <jaujau088@gmail.com>
2022-07-19 16:15:22 +0200

    Create Person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+require 'securerandom'
+
+class Person
+  attr_accessor(:age, :name)
+  attr_reader(:id)
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = SecureRandom.uuid
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
